8c5c6023c7bfab578d279c6e3a1029dd5e66e0c8
Benjamin Zagorski <benj@zagorski.com>
2019-12-19 00:27:16 -0800

    Creating a folder now responds with the folder, not the client

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -9,14 +9,14 @@ class FoldersController < ApplicationController
 
   def create
     client = Client.find(params['client_id'])
-    if client.folders.create({
+    if folder = client.folders.create({
       name: params['name'],
       time_taken: params['time_taken'],
       address: params['address'],
       contact_info: params['contact_info'],
       display_image_id: params['display_image_id']
     })
-      render json:client
+      render json:folder
     else
       render json:{error: 'invalid parameters'}
     end
