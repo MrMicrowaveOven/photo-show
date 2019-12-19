049ff6879d833628bfb329a996e2cba48651272f
Benjamin Zagorski <benj@zagorski.com>
2019-12-19 00:17:44 -0800

    Added most controller actions for clients and folders

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,39 @@
 class ClientsController < ApplicationController
+  def index
+    render json:Client.all
+  end
+
+  def show
+    render json:Client.find(params['id'])
+  end
+
+  def create
+    c = Client.new(name: params['name'])
+    if c.save
+      render json:c
+    else
+      render json:{error: 'invalid parameter'}
+    end
+  end
+
+  def update
+    c = Client.find(params['id'])
+    update_hash = {}
+    ['name'].each do |column_name|
+      if params[column_name]
+        update_hash[column_name] = params[column_name]
+      end
+    end
+    if c.update(update_hash)
+      render json:c
+    else
+      render json:{error: 'invalid parameter'}
+    end
+  end
+
+  def destroy
+    c = Client.find(params['id'])
+    c.delete
+    render json:{client: 'deleted', id: params['id']}
+  end
 end
--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,24 @@
 class FoldersController < ApplicationController
+  def index
+    render json:Folder.all
+  end
+
+  def show
+    render json:Folder.find(params['id'])
+  end
+
+  def create
+    client = Client.find(params['client_id'])
+    if client.folders.create({
+      name: params['name'],
+      time_taken: params['time_taken'],
+      address: params['address'],
+      contact_info: params['contact_info'],
+      display_image_id: params['display_image_id']
+    })
+      render json:client
+    else
+      render json:{error: 'invalid parameters'}
+    end
+  end
 end
